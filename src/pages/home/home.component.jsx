5034543899713b6eadf2dd251e972b823df718b6
Ignacio Pérez Cumillaf <iperezc99@gmail.com>
2021-08-27 14:06:43 -0400

    background hero image was relocated

--- src/pages/home/home.component.jsx
+++ src/pages/home/home.component.jsx
@@ -3,22 +3,14 @@
 import React from 'react';
 import Hero from '../../components/hero/hero.component';
 import Navbar from '../../components/Navbar/Navbar.component';
-import { ThemeProvider, createMuiTheme } from '@material-ui/core';
+import './home.styles.scss'
 
-const theme = createMuiTheme({
-    palette:{
-        primary: {
-            main: '#ff7961'
-        }
-    }
-});
-
-const HomePage = props => {
+const HomePage = () => {
     return (
         <div>
-            <Navbar/>
-            <section className="hero-section">
-                <Hero/>
+            <section className="hero-section-bg">
+                <Navbar/>
+                    <Hero/>
             </section>
         </div>
     );
