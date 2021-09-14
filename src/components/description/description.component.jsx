bda2596f4abcc240c27a6b0910ab8ab2e6774c53
Ignacio Pérez Cumillaf <iperezc99@gmail.com>
2021-09-14 13:26:29 -0300

    mobile and tablet hero views are now responsive

--- src/components/description/description.component.jsx
+++ src/components/description/description.component.jsx
@@ -1,6 +1,6 @@
 import { Typography, Button } from '@material-ui/core';
 import { purple } from '@material-ui/core/colors';
-import { styled } from '@material-ui/core/styles';
+import { makeStyles, styled } from '@material-ui/core/styles';
 import React from 'react';
 import './description.styles.scss';
 
@@ -10,19 +10,94 @@ const PrimaryButton = styled(Button)(({theme}) => ({
     padding: '1.5rem',
     width: '16rem',
     fontSize: '1.3rem'
-
 }));
+//background-position: center??
+//calc(100vh - 82vh) auto; 600p< hacia abajo
+//calc(100vh - 87vh) calc(100vw - 73.5vw); -->600px hacia arriba
+
+const useStyles = makeStyles(theme => ({
+    descriptionContainer: {
+        position: 'absolute',
+        width: '39.8rem',
+        height: '34.6rem',
+        color: '#ffffff',
+        display: 'flex',
+        flexDirection: 'column',
+        fontFamily: 'Manrope',
+        top: '22.5rem',
+        left: '16.5rem',
+        [theme.breakpoints.only('sm')]:{
+            textAlign:'center',
+            width: '38rem',
+            left:'50%',
+            top: '50%',
+            transform: 'translate(-48%, -50%)'
+
+        },   
+        [theme.breakpoints.only('xs')]:{
+            textAlign:'center',
+            top: '50%',
+            left:'50%',
+            width: '32rem',
+            height: '29rem',
+            transform: 'translate(-48%, -40%)'
+        }
+    },
+    newProduct:{
+        opacity: '0.5',
+        fontSize: '1.4rem',
+        lineHeight: '1.9rem',
+        letterSpacing: '1rem',
+        marginBottom: '1.5rem',
+        [theme.breakpoints.only('xs')]:{
+            marginBottom: '1rem'
+        }
+    },
+    productTitle:{
+        fontSize: '5.6rem',
+        fontWeight: 'bold',
+        lineHeight: '5.8rem',
+        letterSpacing: '2px',
+        margin: '.8rem 0',
+        [theme.breakpoints.only('xs')]:{
+            fontSize: '3.6rem',
+            lineHeight: '4rem'
+        }
+    },
+    description:{
+        opacity: '0.75',
+        fontSize: '1.5rem',
+        lineHeight: '2.5rem',
+        fontWeight: '200',
+        width: '34.9rem',
+        margin: '3.3rem 0',
+        [theme.breakpoints.down('sm')]:{
+            margin: '.3rem auto',
+            marginBottom: '2rem'
+        },
+        [theme.breakpoints.only('xs')]:{
+            width:'80%'
+        }
+    },
+    descriptionButton:{
+        [theme.breakpoints.down('sm')]:{
+            margin:'0 auto'
+        }
+    }
+
+}))
 
 
 const Description = props => {
+    const classes = useStyles();
     return (
-        <div className="description-container">
+        <div className={classes.descriptionContainer}>
             
-            <span className="description-container__new-product">NEW PRODUCT</span>
-            <h1 className="description-container__product-title">XX99 MARK II HEADPHONES</h1>
-            <p className="description-container__description">Experience natural, lifelike audio 
+            <span className={classes.newProduct}>NEW PRODUCT</span>
+            <h1 className={classes.productTitle}>XX99 MARK II HEADPHONES</h1>
+            <p className={classes.description}>Experience natural, lifelike audio 
                 and exceptional build quality mode for the passionate music enthusiast.</p>
-            <PrimaryButton>SEE PRODUCT</PrimaryButton>
+            <PrimaryButton className={classes.descriptionButton}>SEE PRODUCT</PrimaryButton>
         </div>
     );
 };
--- src/pages/home/home.component.jsx
+++ src/pages/home/home.component.jsx
@@ -5,6 +5,8 @@ import Hero from '../../components/hero/hero.component';
 import Navbar from '../../components/Navbar/Navbar.component';
 import { makeStyles } from '@material-ui/core';
 import desktopImage from '../../assets/img/home/desktop/image-hero.jpg';
+import tabletDesktopImage from '../../assets/img/home/tablet/image-header.jpg';
+import mobileDesktopImage from '../../assets/img/home/mobile/image-header.jpg';
 
 import './home.styles.scss'
 
@@ -20,9 +22,17 @@ const useStyles = makeStyles((theme) => ({
         backgroundImage: `url(${desktopImage})`,
         backgroundRepeat: 'no-repeat',
         backgroundSize: 'cover',
-        backgroundPosition: 'top',
+        backgroundPosition: 'center',
         [theme.breakpoints.between('md', 'lg')]:{
+            backgroundImage: `url(${desktopImage})`,
             backgroundPosition: '55%'
+        },
+        [theme.breakpoints.only('sm')]:{
+            backgroundImage: `url(${tabletDesktopImage})`,
+            backgroundPosition: 'center'
+         },
+        [theme.breakpoints.only('xs')]:{
+            backgroundImage: `url(${mobileDesktopImage})`
         }
     }
 
